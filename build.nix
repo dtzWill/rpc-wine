93ceb8218ad7b616b69cf52841ffcc1a9a08e096
Will Dietz <w@wdtz.org>
2020-07-23 02:58:56 -0500

    build 64bit, install to $out/lib

--- build.nix
+++ build.nix
@@ -9,5 +9,15 @@ stdenv.mkDerivation {
   nativeBuildInputs = [ cmake ];
 
   buildInputs = [ wine rapidjson ];
+
+  cmakeFlags = [ "-D32BIT=OFF" ];
+
+  installPhase = ''
+    runHook preInstall
+
+    install -D bin64/discord-rpc.dll.so $out/lib/discord-rpc.dll.so
+
+    runHook postInstall
+  '';
 }
 
